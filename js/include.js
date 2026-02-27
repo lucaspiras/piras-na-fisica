e10ff9c0ea448b062ffa265507ca2eee0fb6b559
unknown <piras314@gmail.com>
2026-02-27 11:38:17 -0300

    Corrigindo o header

--- js/include.js
+++ js/include.js
@@ -1,4 +1,8 @@
-fetch("/header/header.html")
+const base = window.location.hostname.includes("github.io")
+  ? "/piras-na-fisica/"
+  : "/";
+
+fetch(base + "header/header.html")
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
